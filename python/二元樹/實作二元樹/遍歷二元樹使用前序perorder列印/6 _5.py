faf684326f4452dfac878b4d1c801b81ff006e87
kelvinng2017 <kelvinngshuhui@gmail.com>
2021-10-17 21:53:46 +0800

    renew

---
+++ python/二元樹/實作二元樹/遍歷二元樹使用前序perorder列印/6 _5.py
@@ -0,0 +1,45 @@
+# /usr/bin/env python
+# -*- coding:UTF-8 -*-
+
+"""
+6_5.py 使用10,21,5,9,13,28系列數字建立一個二元樹，然後使用前序方式列印
+"""
+
+
+class Node():
+    def __init__(self, data=None):
+        '''建立二元樹的節點'''
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insert(self, data):
+        '''建立二元樹'''
+        if self.data:  # 如果根節點存在
+            if data < self.data:  # 插入值小於目前節點值
+                if self.left:
+                    self.left.insert(data)  # 遞迴呼叫往下一層
+                else:
+                    self.left = Node(data)  # 建立新節點存放資料
+            else:  # 插入值大於目前的節點
+                if self.right:
+                    self.right.insert(data)
+                else:
+                    self.right = Node(data)
+        else:
+            self.data = data  # 建立根節點
+
+    def preorder(self):
+        '''前序列印'''
+        print(self.data)  # 列印
+        if self.left:  # 如果左子節點存在
+            self.left.preorder()  # 遞迴呼叫下一層
+        if self.right:  # 如果右子節點存在
+            self.right.preorder()  # 遞迴呼叫下一層
+
+
+tree = Node()  # 建立二元樹物件
+datas = [10, 21, 5, 9, 13, 28]  # 建立二元樹數據
+for d in datas:
+    tree.insert(d)  # 分別插入數據
+tree.preorder()  # 前序列印
